404079d270cd7c7e4ae98e4eb42ac49996af1634
pgherveou <pgherveou@gmail.com>
2023-06-12 10:25:15 +0200

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 #![allow(dead_code)]
 
+mod node_client;
+
 use std::fs::File;
 use std::io::Write;
 
 use anyhow::Result;
-use chrono::prelude::*;
-use codec::Decode;
-use frame_support::pallet_prelude::StorageVersion;
-use frame_support::storage::storage_prefix;
-use serde::Serialize;
-use sp_core::H256;
-use subxt::rpc::types::{StorageChangeSet, StorageData};
+
+use serde::{Serialize, Serializer};
+use subxt::rpc::types::ChainBlock;
+use subxt::rpc::types::{ChainBlockExtrinsic, StorageData};
 use subxt::storage::StorageKey;
-use subxt::{config::PolkadotConfig, OnlineClient};
+use subxt::{Config, PolkadotConfig};
 
 /// Note, generate the file with subxt metadata -f bytes > metadata.scale
 #[subxt::subxt(runtime_metadata_path = "./metadata.scale")]
@@ -24,138 +23,72 @@ struct DBEntry {
     value: Option<StorageData>,
 }
 
+#[derive(Serialize)]
+#[serde(remote = "ChainBlock")]
+pub struct ChainBlockRef<T: Config> {
+    pub header: T::Header,
+
+    #[serde(serialize_with = "vec_chain_block_extrinsic")]
+    pub extrinsics: Vec<ChainBlockExtrinsic>,
+}
+
+pub fn vec_chain_block_extrinsic<S: Serializer>(
+    extrinsics: &Vec<ChainBlockExtrinsic>,
+    serializer: S,
+) -> Result<S::Ok, S::Error> {
+    #[derive(Serialize)]
+    struct Wrapper(#[serde(with = "impl_serde::serialize")] pub Vec<u8>);
+
+    serializer.collect_seq(extrinsics.iter().map(|e| {
+        // https://github.com/paritytech/subxt/issues/1010
+        let raw_bytes = codec::Encode::encode(&e.0);
+        Wrapper(raw_bytes)
+    }))
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    let url = "ws://127.0.0.1:9944";
-    let client = OnlineClient::<PolkadotConfig>::from_url(url).await?;
-    let keys = get_keys(&client, None).await?;
+    let client = node_client::NodeClient::from_url("ws://127.0.0.1:9944").await?;
+    let block_0_hash = client.get_blockhash(0).await?;
 
-    let block_hash = get_blockhash(&client, 0).await?;
-    let change_sets = query_storage_value(&client, keys.clone(), block_hash).await?;
+    // Get the change_sets at block 1
+    {
+        // get all the keys at the last block
+        let keys = client.get_keys(None).await?;
 
-    let json = serde_json::to_string_pretty(&change_sets).unwrap();
-    let mut file = File::create("change_sets.json").unwrap();
-    file.write_all(json.as_bytes()).unwrap();
-
-    let mut db_entries = Vec::new();
-    for key in keys {
-        let value = get_storage_value(&client, &key, None).await?;
-        db_entries.push(DBEntry { key, value });
+        // get change sets for all these keys since block 0
+        let change_sets = client
+            .query_storage_value(keys.clone(), block_0_hash)
+            .await?;
+        let json = serde_json::to_string_pretty(&change_sets).unwrap();
+        let mut file = File::create("change_sets.json").unwrap();
+        file.write_all(json.as_bytes()).unwrap();
     }
 
-    let json = serde_json::to_string_pretty(&db_entries).unwrap();
-    let mut file = File::create("db.json").unwrap();
-    file.write_all(json.as_bytes()).unwrap();
-
-    Ok(())
-}
-
-async fn _print_block_migration(client: &OnlineClient<PolkadotConfig>) -> Result<()> {
-    let mut i = 1;
-    loop {
-        let block_number = get_blocknumber(&client).await? - i;
-        let block_hash = get_blockhash(&client, block_number).await?;
-        let time = get_timestamp(&client, block_hash).await?;
-        let version = get_contract_version(&client, Some(block_hash)).await?;
-        println!("{block_number} -> {time} -> {:?}", version);
-
-        if version == StorageVersion::new(8) {
-            break;
+    // export db at block 0
+    {
+        let keys = client.get_keys(block_0_hash.into()).await?;
+        let mut db_entries = Vec::new();
+        for key in keys {
+            let value = client.get_storage_value(&key, block_0_hash.into()).await?;
+            db_entries.push(DBEntry { key, value });
         }
 
-        i += 1;
+        let json = serde_json::to_string_pretty(&db_entries).unwrap();
+        let mut file = File::create("db.json").unwrap();
+        file.write_all(json.as_bytes()).unwrap();
     }
+
+    // export blocks
+    {
+        #[derive(Serialize)]
+        struct Helper(#[serde(with = "ChainBlockRef")] ChainBlock<PolkadotConfig>);
+
+        let block = client.get_block(None).await?;
+        let json = serde_json::to_string_pretty(&Helper(block)).unwrap();
+        let mut file = File::create("blocks.json").unwrap();
+        file.write_all(json.as_bytes()).unwrap();
+    }
+
     Ok(())
-}
-
-async fn get_blocknumber(client: &OnlineClient<PolkadotConfig>) -> Result<u32> {
-    let number_addr = polkadot::storage().system().number();
-    client
-        .storage()
-        .at_latest()
-        .await?
-        .fetch(&number_addr)
-        .await?
-        .ok_or_else(|| anyhow::format_err!("system::number failed"))
-}
-
-async fn get_contract_version(
-    client: &OnlineClient<PolkadotConfig>,
-    block_hash: Option<H256>,
-) -> Result<StorageVersion> {
-    let key = storage_prefix(b"Contracts", b":__STORAGE_VERSION__:");
-    let StorageData(value) = get_storage_value(client, key, block_hash)
-        .await?
-        .ok_or_else(|| anyhow::format_err!("failed to get contracts storage version"))?;
-
-    StorageVersion::decode(&mut value.as_slice())
-        .ok()
-        .ok_or_else(|| anyhow::format_err!("failed to decode pallet_version"))
-}
-
-async fn get_blockhash(client: &OnlineClient<PolkadotConfig>, block_number: u32) -> Result<H256> {
-    let block_hash_addr = polkadot::storage().system().block_hash(block_number);
-    client
-        .storage()
-        .at_latest()
-        .await?
-        .fetch(&block_hash_addr)
-        .await?
-        .ok_or_else(|| anyhow::format_err!("system::block_hash failed"))
-}
-
-async fn get_timestamp(
-    client: &OnlineClient<PolkadotConfig>,
-    block_hash: H256,
-) -> Result<DateTime<Local>> {
-    let now_addr = polkadot::storage().timestamp().now();
-    let now = client
-        .storage()
-        .at(block_hash)
-        .fetch(&now_addr)
-        .await?
-        .ok_or_else(|| anyhow::format_err!("timestamp::now failed"))?;
-
-    // format timestamp to human readable date
-    let now = NaiveDateTime::from_timestamp_millis(now as i64)
-        .and_then(|now| Local.from_local_datetime(&now).latest())
-        .ok_or_else(|| anyhow::format_err!("failed to convert timestamp to Date"))?;
-
-    Ok(now)
-}
-
-async fn get_keys(
-    client: &OnlineClient<PolkadotConfig>,
-    block_hash: Option<H256>,
-) -> Result<Vec<StorageKey>> {
-    client
-        .rpc()
-        .storage_keys_paged(&[], 100, None, block_hash)
-        .await
-        .map_err(|reason| anyhow::format_err!("get_keys failed: {:?}", reason))
-}
-
-async fn get_storage_value<K: AsRef<[u8]>>(
-    client: &OnlineClient<PolkadotConfig>,
-    key: K,
-    block_hash: Option<H256>,
-) -> Result<Option<StorageData>> {
-    client
-        .rpc()
-        .storage(key.as_ref(), block_hash)
-        .await
-        .map_err(|_| anyhow::format_err!("failed to get storage value"))
-}
-
-async fn query_storage_value(
-    client: &OnlineClient<PolkadotConfig>,
-    keys: Vec<StorageKey>,
-    block_hash: H256,
-) -> Result<Vec<StorageChangeSet<H256>>> {
-    let keys = keys.iter().map(|k| &*k.0);
-    client
-        .rpc()
-        .query_storage(keys, block_hash, None)
-        .await
-        .map_err(|reason| anyhow::format_err!("get_keys failed: {:?}", reason))
 }
---
+++ src/node_client.rs
@@ -0,0 +1,152 @@
+use anyhow::Result;
+use chrono::prelude::*;
+use codec::Decode;
+use frame_support::pallet_prelude::StorageVersion;
+use frame_support::storage::storage_prefix;
+
+use sp_core::H256;
+use subxt::rpc::types::{ChainBlock, ChainBlockResponse, StorageChangeSet, StorageData};
+use subxt::storage::StorageKey;
+use subxt::{config::PolkadotConfig, OnlineClient};
+
+use crate::polkadot;
+
+pub struct NodeClient {
+    client: OnlineClient<PolkadotConfig>,
+}
+
+impl NodeClient {
+    pub async fn from_url(url: &str) -> Result<NodeClient> {
+        let client = OnlineClient::<PolkadotConfig>::from_url(url).await?;
+        Ok(NodeClient { client })
+    }
+
+    /// Print each block until the target version is reached.
+    pub async fn print_block(&self, target_version: StorageVersion) -> Result<()> {
+        let mut block_number = self.get_blocknumber().await?;
+        loop {
+            block_number -= 1;
+            let block_hash = self.get_blockhash(block_number).await?;
+            let time = self.get_timestamp(block_hash).await?;
+            let version = self.get_contract_version(Some(block_hash)).await?;
+            println!("{block_number} -> {time} -> {:?}", version);
+
+            if version == target_version {
+                break;
+            }
+        }
+        Ok(())
+    }
+
+    /// Get the block number of the current block.
+    pub async fn get_blocknumber(&self) -> Result<u32> {
+        let number_addr = polkadot::storage().system().number();
+        self.client
+            .storage()
+            .at_latest()
+            .await?
+            .fetch(&number_addr)
+            .await?
+            .ok_or_else(|| anyhow::format_err!("system::number not found"))
+    }
+
+    /// Get the contract storage version.
+    pub async fn get_contract_version(&self, block_hash: Option<H256>) -> Result<StorageVersion> {
+        let key = storage_prefix(b"Contracts", b":__STORAGE_VERSION__:");
+        let StorageData(value) = self
+            .get_storage_value(key, block_hash)
+            .await?
+            .ok_or_else(|| anyhow::format_err!("contract StorageVersion not found"))?;
+
+        StorageVersion::decode(&mut value.as_slice())
+            .map_err(|reason| anyhow::format_err!("failed to decode StorageVersion: {:?}", reason))
+    }
+
+    /// Get the block hash of the given block number.
+    pub async fn get_blockhash(&self, block_number: u32) -> Result<H256> {
+        let block_hash_addr = polkadot::storage().system().block_hash(block_number);
+        self.client
+            .storage()
+            .at_latest()
+            .await?
+            .fetch(&block_hash_addr)
+            .await?
+            .ok_or_else(|| anyhow::format_err!("system::block_hash {block_number} not found"))
+    }
+
+    /// Get the timestamp of the given block.
+    pub async fn get_timestamp(&self, block_hash: H256) -> Result<DateTime<Local>> {
+        let now_addr = polkadot::storage().timestamp().now();
+        let now = self
+            .client
+            .storage()
+            .at(block_hash)
+            .fetch(&now_addr)
+            .await?
+            .ok_or_else(|| anyhow::format_err!("timestamp::now not found"))?;
+
+        // format timestamp to human readable date
+        let now = NaiveDateTime::from_timestamp_millis(now as i64)
+            .and_then(|now| Local.from_local_datetime(&now).latest())
+            .ok_or_else(|| anyhow::format_err!("failed to convert timestamp to Date"))?;
+
+        Ok(now)
+    }
+
+    /// Get all the keys in storage at the given block.
+    pub async fn get_keys(&self, block_hash: Option<H256>) -> Result<Vec<StorageKey>> {
+        const PAGE_SIZE: usize = 100;
+        let mut keys = Vec::<StorageKey>::new();
+
+        loop {
+            let new_keys = self
+                .client
+                .rpc()
+                .storage_keys_paged(&[], PAGE_SIZE as u32, None, block_hash)
+                .await
+                .map_err(|reason| anyhow::format_err!("get_keys failed: {:?}", reason))?;
+
+            let has_more = new_keys.len() > PAGE_SIZE;
+            keys.extend(new_keys);
+            if !has_more {
+                break;
+            }
+        }
+
+        Ok(keys)
+    }
+
+    pub async fn get_storage_value<K: AsRef<[u8]>>(
+        &self,
+        key: K,
+        block_hash: Option<H256>,
+    ) -> Result<Option<StorageData>> {
+        self.client
+            .rpc()
+            .storage(key.as_ref(), block_hash)
+            .await
+            .map_err(|err| anyhow::format_err!("failed to get storage value: {:?}", err))
+    }
+
+    pub async fn query_storage_value(
+        &self,
+        keys: Vec<StorageKey>,
+        block_hash: H256,
+    ) -> Result<Vec<StorageChangeSet<H256>>> {
+        let keys = keys.iter().map(|k| &*k.0);
+        self.client
+            .rpc()
+            .query_storage(keys, block_hash, None)
+            .await
+            .map_err(|err| anyhow::format_err!("get_keys failed: {:?}", err))
+    }
+
+    pub async fn get_block(&self, block_hash: Option<H256>) -> Result<ChainBlock<PolkadotConfig>> {
+        self.client
+            .rpc()
+            .block(block_hash)
+            .await?
+            .ok_or_else(|| anyhow::format_err!("block not found"))
+            .map(|ChainBlockResponse { block, .. }| block)
+    }
+}
