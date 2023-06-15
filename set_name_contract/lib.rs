b2124bec3c194db1ec6bf67b12dba3673b0e5c02
pgherveou <pgherveou@gmail.com>
2023-06-15 14:30:01 +0200

    add cargo-contract

---
+++ set_name_contract/lib.rs
@@ -0,0 +1,37 @@
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
+
+#[ink::contract]
+mod name_setter {
+    use ink::prelude::string::String;
+
+    #[ink(storage)]
+    pub struct NameSetter {
+        name: String,
+    }
+
+    impl NameSetter {
+        /// Constructor that initializes the `name` value to the given `init_value`.
+        #[ink(constructor)]
+        pub fn new(init_value: String) -> Self {
+            Self { name: init_value }
+        }
+
+        /// Constructor that initializes the `name` value to the empty string.
+        #[ink(constructor)]
+        pub fn default() -> Self {
+            Self::new(Default::default())
+        }
+
+        /// Set the `name` to the given value.
+        #[ink(message)]
+        pub fn set_name(&mut self, new_value: String) {
+            self.name = new_value;
+        }
+
+        /// Simply returns the current `name` value.
+        #[ink(message)]
+        pub fn get_name(&self) -> String {
+            self.name.clone()
+        }
+    }
+}
