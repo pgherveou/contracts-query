2db6e54605c6bb22e67f43af18d01495b5b48e96
pgherveou <pgherveou@gmail.com>
2023-06-19 15:47:03 +0200

    fix readme

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@
 //!
 //! Print each block until the target version is reached.
 //! ```bash
-//! > contracts-query "wss://rococo-contracts-rpc.polkadot.io:443" print-migrating-blocks --target-version 8
+//! > contracts-query --url "wss://rococo-contracts-rpc.polkadot.io:443" print-migrating-blocks --target-version 8
 //! Fetching migration blocks:
 //! 2023-06-05 18:29:00.138 +02:00 -> BlockInfo { block_hash: 0xc5a879739b995b8b69655607f3ae59f8707c6e92026e953f9d474131808cf9e1, block_number: 2738932, version: 10, migration_in_progress: true }
 //! 2023-06-05 18:28:48.079 +02:00 -> BlockInfo { block_hash: 0x27224b9b37a031bedf507fa37c0aad108480a711a1b5ab572aa3a7680aa79bc8, block_number: 2738931, version: 9, migration_in_progress: true }
